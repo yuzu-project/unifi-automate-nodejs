0f3eef54404f4ad99aede06b2ebf4c04e965db49
Renato Yamassaki <renato@yuzu-project.com>
2020-04-18 22:46:57 -0300

    Create winston logger

---
+++ src/util/logger.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+
+class logger {
+  constructor() {
+    this.innerLogger = winston.createLogger({
+      transports: [
+        new winston.transports.Console(),
+      ],
+      format: winston.format.combine(
+        winston.format.json(),
+        winston.format.splat(),
+        winston.format.colorize(),
+      ),
+    });
+  }
+
+  info(message, ...additionalInfo) {
+    this.innerLogger.info(message, additionalInfo);
+  }
+
+  verbose(message, ...additionalInfo) {
+    this.innerLogger.verbose(message, additionalInfo);
+  }
+
+  warning(message, ...additionalInfo) {
+    this.innerLogger.warning(message, additionalInfo);
+  }
+
+  error(message, ...additionalInfo) {
+    this.innerLogger.error(message, additionalInfo);
+  }
+}
+
+export default logger;
